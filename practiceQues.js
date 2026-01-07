da3b8dc41a47ee0591c5631f97a01b0d9aaa38fe
Devendra Kumar Narware <devn2405@gmail.com>
2026-01-07 07:10:54 +0530

    Practice more Basic question
    Refactor code by commenting out unused sections and improving readability.

--- practiceQues.js
+++ practiceQues.js
@@ -1,21 +1,72 @@
 // question on conditionals
-// if-else , loops etc
-let a = Number(prompt("enter a number:"));
-// let b = Number(prompt("enter a number:"));
 
 //question - 1
-if(a>10){
-    console.log("hello")
-}else{
-    console.log("hey")
-}
+//how to take input
+/*
+
+    let a = Number(prompt("enter a number:"));
+    if(a>10){
+        console.log("hello")
+    }else{
+        console.log("hey")
+    }
+
+*/
+
 
 
 //question - 2 
-let num = a + 10 ;
+//string concatenation and new method to take input
+/*
+    let a = Number(prompt("enter a number:"));
+    let num = a + 10 ;
 
-if(num>20){
-    console.log("hello")
+    if(num>20){
+        console.log("hello")
+    }else{
+        console.log("hey")
+    }
+
+*/
+
+
+//question - 3
+//use of ===
+
+    // let password = "Dev*123"
+    // let pass = prompt("Enter the password: ") ;
+    // if(pass == password){
+    //     console.log("login successfull") ;
+    // }else{
+    //     console.log("access denied ❌") ;
+    // }
+
+
+//question - 4
+//even odd
+
+    // let num = Number(prompt("Enter a number: ")) ;
+    // if(num%2 == 0){
+    //     console.log("number is EVEN") ;
+    // }else{
+    //     console.log("number is ODD") ;
+    // }
+
+
+//question - 5
+//grading system
+
+let marks = Number(prompt("Enter your marks: ")) ;
+if(marks > 100 ){
+    console.log("Invalid Input") ;
+}else if(marks > 85){
+    console.log("A+") ;
+}else if(marks > 70){
+    console.log("A") ;
+}else if(marks > 50 ){
+    console.log("B") ;
+}else if(marks > 33){
+    console.log("C") ;
 }else{
-    console.log("hey")
+    console.log("FAIL") ;
 }
