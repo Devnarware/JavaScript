d4c68b83b651814246650cdee33de358ba19e8cb
Devendra Kumar Narware <devn2405@gmail.com>
2026-01-07 06:38:10 +0530

    practicing some question on js

---
+++ practiceQues.js
@@ -0,0 +1,21 @@
+// question on conditionals
+// if-else , loops etc
+let a = Number(prompt("enter a number:"));
+// let b = Number(prompt("enter a number:"));
+
+//question - 1
+if(a>10){
+    console.log("hello")
+}else{
+    console.log("hey")
+}
+
+
+//question - 2 
+let num = a + 10 ;
+
+if(num>20){
+    console.log("hello")
+}else{
+    console.log("hey")
+}
