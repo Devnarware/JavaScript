bc9ead2ef2c1c8eebc5cd13dd3fdab1c19d80773
Devendra Kumar Narware <devn2405@gmail.com>
2026-03-02 00:21:47 +0530

    learning about api calls

--- AdvanceOne/script.js
+++ AdvanceOne/script.js
@@ -1,6 +1,32 @@
-const requestUrl = 'https://api.github.com/users/Devnarware' ;
+const upper = document.querySelector(".upper") ;
+const userName = document.getElementById("name") ;
+const follower = document.getElementById("follower") ;
+const repoCount = document.getElementById("repoCount") ;
+
+
+const requestUrl = 'https://api.github.com/users/hiteshchoudhary' ;
 const xhr = new XMLHttpRequest();
+
+
+
 xhr.open('GET', requestUrl)
+xhr.send() ;
 xhr.onreadystatechange = function (){
-    console.log(xhr.readyState) ;
+    
+    if(xhr.readyState === 4){
+
+        const data = JSON.parse(this.responseText) ;
+
+
+        upper.style.backgroundImage = `url(${data.avatar_url})` ;
+        userName.innerText = `${data.name}`
+        follower.innerText = `${data.followers}`
+        repoCount.innerText = `${data.public_repos}`
+
+
+    }
+    
 }
+
+
+
