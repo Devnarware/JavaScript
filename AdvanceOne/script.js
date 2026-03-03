4d7ae811cb262b11e378d17bb4d691c064d8b0b4
Devendra Kumar Narware <devn2405@gmail.com>
2026-03-03 23:37:36 +0530

    learning about api calls

--- AdvanceOne/script.js
+++ AdvanceOne/script.js
@@ -2,9 +2,32 @@ const upper = document.querySelector(".upper") ;
 const userName = document.getElementById("name") ;
 const follower = document.getElementById("follower") ;
 const repoCount = document.getElementById("repoCount") ;
+const input = document.querySelector("input");
+const form = document.querySelector("form");
+let userInput ;
+const box = document.querySelector(".box") ;
+
+input.addEventListener("keydown" , function(e){
+    if(e.key === "Enter"){
+        e.preventDefault() ;
+        form.requestSubmit() ;
+    }
+})
+form.addEventListener("submit" , function(e){
+    e.preventDefault() ;
+    userInput = input.value ;
+    getApi() ;
+    form.style.display = "none"
+    box.style.display = "flex"
+    
 
 
-const requestUrl = 'https://api.github.com/users/hiteshchoudhary' ;
+
+
+}) ;
+
+function getApi(){
+    const requestUrl = `https://api.github.com/users/${userInput}` ;
 const xhr = new XMLHttpRequest();
 
 
@@ -30,3 +53,5 @@ xhr.onreadystatechange = function (){
 
 
 
+
+}
