6c522686b03c18e18f434bfee27ca67882f04190
Devendra Kumar Narware <devn2405@gmail.com>
2026-03-06 00:38:50 +0530

    learning about api handlining

--- AdvanceOne/script.js
+++ AdvanceOne/script.js
@@ -1,57 +1,51 @@
-const upper = document.querySelector(".upper") ;
-const userName = document.getElementById("name") ;
-const follower = document.getElementById("follower") ;
-const repoCount = document.getElementById("repoCount") ;
+const upper = document.querySelector(".upper");
+const userName = document.getElementById("name");
+const follower = document.getElementById("follower");
+const repoCount = document.getElementById("repoCount");
 const input = document.querySelector("input");
 const form = document.querySelector("form");
-let userInput ;
-const box = document.querySelector(".box") ;
+let userInput;
+const box = document.querySelector(".box");
 
-input.addEventListener("keydown" , function(e){
-    if(e.key === "Enter"){
-        e.preventDefault() ;
-        form.requestSubmit() ;
+input.addEventListener("keydown", function (e) {
+    if (e.key === "Enter") {
+        e.preventDefault();
+        form.requestSubmit();
     }
 })
-form.addEventListener("submit" , function(e){
-    e.preventDefault() ;
-    userInput = input.value ;
-    getApi() ;
+form.addEventListener("submit", function (e) {
+    e.preventDefault();
+    userInput = input.value;
+    getApi();
     form.style.display = "none"
     box.style.display = "flex"
-    
 
 
+});
+
+function getApi() {
+    const requestUrl = `https://api.github.com/users/${userInput}`;
+    const xhr = new XMLHttpRequest();
 
 
-}) ;
+    xhr.open('GET', requestUrl)
+    xhr.send();
+    xhr.onreadystatechange = function () {
 
-function getApi(){
-    const requestUrl = `https://api.github.com/users/${userInput}` ;
-const xhr = new XMLHttpRequest();
+        if (xhr.readyState === 4) {
 
-
-
-xhr.open('GET', requestUrl)
-xhr.send() ;
-xhr.onreadystatechange = function (){
-    
-    if(xhr.readyState === 4){
-
-        const data = JSON.parse(this.responseText) ;
+            const data = JSON.parse(this.responseText);
 
 
-        upper.style.backgroundImage = `url(${data.avatar_url})` ;
-        userName.innerText = `${data.name}`
-        follower.innerText = `${data.followers}`
-        repoCount.innerText = `${data.public_repos}`
+            upper.style.backgroundImage = `url(${data.avatar_url})`;
+            userName.innerText = `${data.name}`
+            follower.innerText = `${data.followers}`
+            repoCount.innerText = `${data.public_repos}`
 
+
+        }
 
     }
-    
-}
-
-
 
 
 }
